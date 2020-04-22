7feaa19e58c4ba125ab671ec581d6ff99c17e802
diegolt8 <diegoalugo@hotmail.com>
2020-04-21 19:07:13 -0500

    Proveedor y inventorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { InventaryComponent } from './pages/manager/inventary/inventary.component';
+import { LaboratoriesComponent } from './pages/manager/laboratories/laboratories.component';
+import { ProvidersComponent } from './pages/manager/providers/providers.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'venta', component: VentaComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'inventario', component: InventaryComponent },
+  { path: 'laboratorio', component: LaboratoriesComponent },
+  { path: 'proveedor', component: ProvidersComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './pages/login/login.component';
 import { InventaryComponent } from './pages/manager/inventary/inventary.component';
 import { ProvidersComponent } from './pages/manager/providers/providers.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { SalesComponent } from './pages/manager/sales/sales.component';
+import { UsersComponent } from './pages/manager/users/users.component';
+import { ShelfsComponent } from './pages/manager/shelfs/shelfs.component';
+import { LaboratoriesComponent } from './pages/manager/laboratories/laboratories.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +22,15 @@ import { CarouselComponent } from './components/carousel/carousel.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent,
     InventaryComponent,
     ProvidersComponent,
-    CarouselComponent
+    CarouselComponent,
+    VentaComponent,
+    CarritoComponent,
+    SalesComponent,
+    UsersComponent,
+    ShelfsComponent,
+    LaboratoriesComponent
   ],
   imports: [
     BrowserModule,
